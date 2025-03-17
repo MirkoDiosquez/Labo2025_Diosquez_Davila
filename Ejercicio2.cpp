8733bead14b8763c69b9399257c2fc8e61d27320
Tiago-Davila <tiagodavila08@gmail.com>
2025-03-17 12:41:33 -0300

    Se suben los ejercicios

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,77 +1,116 @@
 #include <iostream>
 #include <vector>
-using namespace std ;
+using namespace std;
 
-struct productos_t 
-{
-    string nombre ;
-    int codigo ;
-    string marca ;
-    int precio ;
-    bool vencimiento ;
+struct fecha_t {
+    int dia;
+    int mes;
+    int anio;
 };
 
-void cargadatos (vector<vector<productos_t>> & gondola )
-{
-    for(int i = 0 ; i < gondola.size() ; i ++)
-    {
-        for(int j = 0 ; gondola[i].size() ; j++)
-        {
-            cout << "Ingrese el nombre del producto de la fila " << i << " y de la columna " << j << endl ;
-            cin >> gondola[i][j].nombre ;
-            cout << "Ingrese el codigo del producto de la fila " << i << " y de la columna " << j << endl ;
-            cin >> gondola[i][j].codigo ;
-            cout << "Ingrese la marca del producto de la fila " << i << " y de la columna " << j << endl ;
-            cin >> gondola[i][j].marca ;
-            cout << "Ingrese el precio del producto de la fila " << i << " y de la columna " << j << endl ;
-            cin >> gondola[i][j].precio ;
-            cout << "Ingrese si el producto de la fila " << i << " y de la columna " << j << "tiene fecha de vencimiento " << endl ;
-            cin >> gondola[i][j].vencimiento ;
+struct productos_t {
+    string nombre;
+    int codigo;
+    string marca;
+    float precio;
+    bool vencimiento;
+    fecha_t fecha;
+};
+
+int columnapreciomayor(vector<vector<productos_t>>  gondola) {
+    int totalColumna = 0;
+    int mayorPrecio = 0;
+    int ColumnaMayor = 0;
+    for (int j = 0; j < 4; j++) {
+        totalColumna = 0;
+        for (int i = 0; i < 3; i++) {
+            totalColumna += gondola[i][j].precio;
+        }
+        if (totalColumna > mayorPrecio) {
+            mayorPrecio = totalColumna;
+            ColumnaMayor = j;
         }
     }
+    return ColumnaMayor;    
 }
 
-void columnaprecio( vector<vector<productos_t>>  gondola )
+void nombre(vector<vector<productos_t>>  gondola) {
+    cout << "El nombre del producto de la fila 2 columna 3 es: " 
+         << gondola[2][3].nombre << endl;
+}
+
+void promedio(vector<vector<productos_t>>  gondola) {
+    float auxi = 0;
+    for (int i = 0; i < gondola.size(); i++) {
+        for (int j = 0; j < gondola[i].size(); j++) {
+            auxi += gondola[i][j].precio;
+        }
+    }
+    auxi /= 12;
+    cout << "El promedio de valor de los productos es de: " << auxi << endl;
+}
+int cantProductosVenc (vector<vector<productos_t>> gondola)
 {
-    int auxi = 0 ;
-    for(int i = 0 ; i < gondola.size(); i ++ )
+    int auxi = 0 ; 
+    for (int i = 0; i < gondola.size(); i++) 
     {
-        for(int j = 0 ; gondola[i].size() ; j++)
+        for (int j = 0; j < gondola[i].size(); j++) 
         {
-            /*if(gondola[j][i])    
+            if (gondola[i][j].vencimiento == true)
             {
-
-            }*/
+                auxi ++ ;
+            }
         }
     }
+    cout << "La cantidad de productos con fecha de vencimiento es de:" << auxi << endl ;
+    return auxi ;
+}   
 
-}
+int main() {
+    vector<vector<productos_t>> gondola(3, vector<productos_t>(4)) ;
 
-void nombre (vector<vector<productos_t>>  gondola)
-{
-    cout << "El nombre del producto de la fila 2 columna 3 es " << gondola[2][3].nombre << endl ;
-}
+    gondola[0][0] = {"Leche Entera", 101, "La Serenísima", 59.90, false, {}};
+    gondola[0][1] = {"Pan de Molde", 102, "Bimbo", 119.50, true, {20, 3, 2025}};
+    gondola[0][2] = {"Jugo de Naranja", 103, "TetraPak", 110.00, false, {}};
+    gondola[0][3] = {"Yogurt Natural", 104, "Danone", 79.90, true, {5, 12, 2025}};
 
-void promedio (vector<vector<productos_t>>  gondola)
-{
-    int auxi = 0 ;
-    for(int i = 0 ; i < gondola.size() ; i ++)
-    {
-        for(int j = 0 ; gondola[i].size() ; j++)
-        {
-            auxi = auxi + gondola[i][j].precio ;
-        }
-    }
-    auxi = auxi/12;
-    cout << "El promedio de valor de los productos es de " << auxi << endl ;
-}
+    gondola[1][0] = {"Aceite de Oliva", 105, "La Española", 299.99, false, {}};
+    gondola[1][1] = {"Cereal Integral", 106, "Kellogg's", 180.75, true, {25, 8, 2023}};
+    gondola[1][2] = {"Galletitas de Chocolate", 107, "Chocolinas", 85.00, false, {}};
+    gondola[1][3] = {"Arroz Integral", 108, "Luchito", 115.50, true, {2, 1, 2026}};
 
-int main()
-{
+    gondola[2][0] = {"Agua Mineral", 109, "Villa del Sur", 40.00, false, {}};
+    gondola[2][1] = {"Cerveza", 110, "Quilmes", 150.50, true, {7, 4, 2024}};
+    gondola[2][2] = {"Papel Higiénico", 111, "Regio", 125.00, false, {}};
+    gondola[2][3] = {"Detergente Líquido", 112, "Skip", 99.90, true, {23, 9, 2024}};
+
+    int aux = 0;
     
-    vector<vector<productos_t>> gondola[3][4] ;
-    cargadatos(gondola[3][4]);
-    columnaprecio(gondola[3][4]);   
-    nombre(gondola[3][4]);
-    promedio(gondola[3][4]);
+    while (true) {
+        cout << "Ingrese 1 para ver la columna de la gondola que tiene mayor precio" << endl;
+        cout << "Ingrese 2 para ver el nombre del producto de la fila 2 y columna 3" << endl;
+        cout << "Ingrese 3 para ver el promedio del precio de la gondola" << endl;
+        cout << "Ingrese 4 para ver la cantidad de productos con fecha de vencimiento" << endl;
+        cout << "Ingrese 5 para finalizar el programa" << endl;
+        cin >> aux;
+
+        switch (aux) 
+        {
+            case 1:
+                cout << "La columna con mayor precio es: " << columnapreciomayor(gondola) << endl;
+                break;
+            case 2:
+                nombre(gondola);
+                break;
+            case 3:
+                promedio(gondola);
+                break;
+            case 4:
+                cantProductosVenc(gondola);
+                break;
+            case 5:
+                cout << "Programa finalizado" ;
+                return 0 ;
+        }
+    }
 }
--- Ejercicios5.cpp
+++ Ejercicios5.cpp
@@ -5,6 +5,7 @@ struct Producto{
     string nombre;
     bool promocion;
     int cantVentas;
+    int precio;
     string categoria ;
 };
 struct fecha_t {
@@ -14,7 +15,7 @@ struct fecha_t {
 };
 struct Pedido{
     string nombre;
-    string productos;
+    Producto productos;
     int cantidad;
     int precio;
     fecha_t fecha;
@@ -28,10 +29,14 @@ void insertar(vector<Producto> &productos){
     while (n == 1) {
         cout << "Ingrese nombre del producto: "<<endl;
         cin >> producto.nombre;
-        cout << "Ingrese si esta en promocion: "<<endl;
+        cout << "Ingrese el precio del producto "<<endl;
+        cin >> producto.precio;
+        cout << "Ingrese si esta en promocion 0 para no/1 para si : "<<endl;
         cin >> producto.promocion;
-        cout << "Ingrese la cantidad de ventas: "<<endl;
-        cin >> producto.cantVentas;
+        if(producto.promocion == true){
+            producto.precio = producto.precio-(producto.precio * 0.2);
+        }
+       
         cout << "Ingrese la categoria del producto: "<<endl;
         cin >> producto.categoria;
         productos.push_back(producto);
@@ -42,29 +47,45 @@ void insertar(vector<Producto> &productos){
 }
 void realizarPedido(vector<Pedido> &pedidos, vector<Producto> &productos){
     Pedido pedido;
+    Producto producto;
     int n=0;
+    int cantidad = 0;
+    int total = 0;
     cout << "1 para insertar datos o 2 para salir: "<<endl;
     cin >> n;
+    
     while(n==1){
         cout << "Ingrese nombre del cliente: "<<endl;
         cin >> pedido.nombre;
-        cout << "Ingrese el producto: "<<endl;
-        cin >> pedido.productos;
-        cout << "Ingrese la cantidad: "<<endl;
+        cout<< "Dime la cantidad de productos que deseas comprar: "<<endl;
         cin >> pedido.cantidad;
-        cout << "Ingrese el precio: "<< endl;
-        cin >> pedido.precio;
+        for (int i = 0; i < pedido.cantidad; i++) {
+            Producto producto;
+            cout << "Ingrese nombre del producto: " << endl;
+            cin >> producto.nombre;
+            
+
+            for (int j = 0; j < productos.size(); j++) {
+                if (producto.nombre == productos[j].nombre) {
+                    pedido.productos.nombre = productos[j].nombre;
+                    pedido.precio =pedido.precio+ productos[j].precio;
+                    pedido.productos.categoria = productos[j].categoria;
+                    pedido.productos.promocion = productos[j].promocion;
+                    productos[j].cantVentas++;
+                    
+                    
+                }
+            }
+
+        }
+        
         cout << "Ingrese dia de la fecha: "<< endl;
         cin >> pedido.fecha.dia; 
         cout << "Ingrese mes de la fecha: "<< endl;
         cin >> pedido.fecha.mes; 
         cout << "Ingrese anio de la fecha: "<< endl;
         cin >> pedido.fecha.anio; 
-        if(productos.promocion= true){
-            cout<<"El precio total es: "<<pedido.cantidad*pedido.precio*1.2<<endl;
-        }
-        else
-        cout<<"El precio total es: "<<pedido.cantidad*pedido.precio<<endl;
+        cout << "El total de la compra es: " << pedido.precio << endl;
 
         pedidos.push_back(pedido);
         cout << "1 para insertar datos o 2 para salir: " << endl;
@@ -72,11 +93,77 @@ void realizarPedido(vector<Pedido> &pedidos, vector<Producto> &productos){
         cin.ignore();
 }
 }
+void pedidoCliente(vector <Pedido> &pedidos){
+    string nombre;
+    cout << "Dime el nombre del cliente: ";
+    cin >> nombre;
+    for (int i = 0; i < pedidos.size(); i++)
+    {
+        if(nombre == pedidos[i].nombre){
+            cout << "Nombre del cliente: " << pedidos[i].nombre << endl;
+            cout << "Nombre del producto/s: " << pedidos[i].productos.nombre << endl;
+            cout << "Cantidad: " << pedidos[i].cantidad << endl;
+            cout << "Precio: " << pedidos[i].precio << endl;
+            cout << "Fecha: " << pedidos[i].fecha.dia << "/" << pedidos[i].fecha.mes << "/" << pedidos[i].fecha.anio << endl;
+        }
+    }
+}
+void imprimir(vector <Producto> productos){
+    for (int i = 0; i < productos.size(); i++)
+    {
+        cout << "Nombre del producto: " << productos[i].nombre << endl;
+        cout << "Cantidad de ventas: " << productos[i].cantVentas << endl;
+        cout << "Categoria: " << productos[i].categoria << endl;
+        cout << "Cantidad de ventas" << productos[i].cantVentas << endl;
+}}
+void ordenarMayorVentas(vector<Producto> &productos){
+    Producto aux;
+    for (int i = 0; i < productos.size(); i++)
+    {
+        for (int j = 0; j < productos.size(); j++)
+        {
+            if(productos[i].cantVentas > productos[j].cantVentas){
+                aux = productos[i];
+                productos[i] = productos[j];
+                productos[j] = aux;
+                imprimir(productos);
+            }
+        }
+    }
+   
+}
 
-int main(){
+int main() {
     vector<Pedido> pedidos;
     vector<Producto> productos;
-    insertar(productos);
-    return 0;
+    int opcion = 0;
 
+    while (opcion != 5) {
+        cout << "Quieres ingresar Productos - 1" << endl
+             << "Quieres ingresar Pedidos - 2" << endl
+             << "Quieres Mostrar Pedidos por nombre del cliente - 3" << endl
+             << "Quieres mostrar productos por cantidad de ventas - 4" << endl
+             << "Salir - 5" << endl;
+        cin >> opcion;
+        switch (opcion) {
+            case 1:
+                insertar(productos);
+                break;
+            case 2:
+                realizarPedido(pedidos, productos);
+                break;
+            case 3:
+                pedidoCliente(pedidos);
+                break;
+            case 4:
+                ordenarMayorVentas(productos);
+                break;
+            case 5:
+                return 0;
+            default:
+                cout << "Opcion no valida." << endl;
+                break;
+        }
+    }
+    return 0;
 }
