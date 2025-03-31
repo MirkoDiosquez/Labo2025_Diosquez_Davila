bc51ec7687d84f139de5d4d9443a86f6ac41064e
Tiago-Davila <tiagodavila08@gmail.com>
2025-03-31 12:43:26 -0300

    ejercicios Davila

---
+++ LaboratorioPOO/src/carpetaJavaDavila/numeroMayor.java
@@ -0,0 +1,12 @@
+public class numeroMayor {
+    public static void main(String[] args) {
+        int variable1=90;
+        int variable2=30;
+        if (variable1==variable2){
+            System.out.println("Las variables son iguales");
+        } else if (variable1!=variable2) {
+            System.out.println("Las variables no son iguales");
+        }
+
+    }
+}
