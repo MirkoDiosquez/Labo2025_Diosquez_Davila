18602d7170e05f7194901fdf4ad505ef4872b50c
MirkoDiosquez <alumno26.diosquez.mirko@ipm.edu.ar>
2025-04-28 09:07:19 -0300

    dada

--- LaboratorioPOO-Diosquez/src/Unidad2/CD.java
+++ LaboratorioPOO-Diosquez/src/Unidad2/CD.java
@@ -4,6 +4,7 @@ package Unidad2;
 import Unidad1.Cancion;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CD {
     private ArrayList<Cancion> canciones ;
@@ -21,14 +22,57 @@ public class CD {
     }
 
     // Getters
-    public ArrayList<Cancion> listaDeCanciones ()
+    public ArrayList<Cancion> getCanciones ()
     {
         return canciones ;
     }
-    
+
+    //Setter
+    public void setCanciones(ArrayList<Cancion> canciones)
+    {
+        this.canciones = canciones;
+    }
+
+    public int numeroCanciones()
+    {
+       int auxi = 0 ;
+        for (Cancion lababa : canciones)
+        {
+            auxi += 1 ;
+        }
+       return auxi;
+    }
+
+    public String verCancion(int posicion)
+    {
+        if (posicion >= 0 && posicion < canciones.size() )
+        {
+            Cancion auxx = canciones.get(posicion) ;
+            return "La canción en la posicion " +posicion +" es: " +auxx.getTitulo() ;
+        }
+        else
+        {
+        return "Posicion invalida" ;
+        }
+    }
+
+    public void grabaCancion(int posicion, Cancion nuevaCancion)
+    {
+        Cancion auxi = canciones.get(posicion) ;
+        canciones.set(posicion, nuevaCancion) ;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println();
+        CD c1 = new CD () ;
+        Scanner entrada = new  Scanner(System.in) ;
+        int posicion = 0 ;
+        System.out.println("La cantidad de canciones dentro del CD: " +c1.numeroCanciones()) ;
+        System.out.println("Introduzca una posicion de cancion de CD para saber cual es");
+        posicion = entrada.nextInt() ;
+
+
+
     }
 
 
