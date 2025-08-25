12d67d9fc4fc959596c24fc203495604f0ef9c16
Tiago-Davila <tiagodavila08@gmail.com>
2025-08-25 12:05:28 -0300

    ej 2 terminado empezando el 4

--- Davila_POO/src/SistemaAlarmaExp/MainAlarm.java
+++ Davila_POO/src/SistemaAlarmaExp/MainAlarm.java
@@ -1,14 +1,36 @@
 package SistemaAlarmaExp;
 
+import Alarmas.Dispositivo;
+import Alarmas.SensorPresion;
+import Alarmas.SensorTemperatura;
 import Alarmas.Sistema;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainAlarm {
-    public static void main(String[] args) {
-        Sistema sis = new Sistema();
+    public Dispositivo elegirDispositivo(Sistema sis) throws Exception {
         Scanner s = new Scanner(System.in);
-        int alarma = s.nextInt();
+        System.out.println("Elige un numero para la alarma");
+        int indice = s.nextInt();
+        Dispositivo alarma = sis.getDispositivos().get(indice);
+        return alarma;
     }
 
+    public static void main(String[] args) {
+        MainAlarm Asis = new MainAlarm();
+        Sistema sis = new Sistema();
+        ArrayList<Dispositivo> alarmas = new ArrayList<>();
+        LocalDate fechaActual = LocalDate.now();
+        SensorTemperatura d1 = new SensorTemperatura(true, 2.5,3.5, fechaActual);
+        SensorPresion d2 = new SensorPresion(true, 2.5,3.5, fechaActual);
+        try {
+            Asis.elegirDispositivo(sis);
+        } catch (Exception err) {
+            System.out.println("Ocurrio alguna excepcion");
+        }
+
+    }
 }
